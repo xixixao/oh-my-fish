73661b9e15f2b801120353ff7af6d5361820cfe6
xixixao <xixixao@seznam.cz>
2013-12-20 05:12:59 +0100

    Minimal theme

---
+++ themes/minimal/fish_prompt.fish
@@ -0,0 +1,39 @@
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _number_dirty_files
+  echo (command git status -s --ignore-submodules=dirty ^/dev/null | wc -l)
+end
+
+begin
+
+  function fish_prompt
+    set -l cyan (set_color 0cf)
+    set -l normal (set_color normal)
+
+    set -l cwd $cyan(basename (prompt_pwd))
+
+    echo -n -s $cwd $git_info $normal ' '
+  end
+
+  function fish_right_prompt
+    set -l yellow (set_color ff0)
+    set -l cyan (set_color 0cf)
+    set -l normal (set_color normal)
+
+    if [ (_git_branch_name) ]
+      set -l git_branch (_git_branch_name)
+      set git_info "$cyan$git_branch"
+
+      set -l n (_number_dirty_files)
+      if [ $n -gt 0 ]
+        set -l dirty "$yellow$n"
+        set git_info "$dirty $git_info"
+      end
+    end
+
+    echo -n -s $git_info
+  end
+
+end
