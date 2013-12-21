73eda25a7fdcad072f5abdc38da5a6735e6b8762
xixixao <xixixao@seznam.cz>
2013-12-21 23:24:42 +0100

    Seperated number of untracked files

--- themes/minimal/fish_prompt.fish
+++ themes/minimal/fish_prompt.fish
@@ -2,8 +2,12 @@ function _git_branch_name
   echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
 end
 
-function _number_dirty_files
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null | wc -l)
+function _number_modified_files
+  echo (command git status -s -uno --ignore-submodules=dirty ^/dev/null | wc -l)
+end
+
+function _number_untracked_files
+  echo (command git ls-files --other --exclude-standard ^/dev/null | wc -l | tr -d ' ')
 end
 
 begin
@@ -20,6 +24,7 @@ begin
 
   function fish_right_prompt
     set -l yellow (set_color ff0)
+    set -l gray (set_color 999)
     set -l cyan (set_color 0cf)
     set -l normal (set_color normal)
 
@@ -27,9 +32,15 @@ begin
       set -l git_branch (_git_branch_name)
       set git_info "$cyan$git_branch"
 
-      set -l n (_number_dirty_files)
-      if [ $n -gt 0 ]
-        set -l dirty "$yellow$n"
+      set -l nu (_number_untracked_files)
+      if [ $nu -gt 0 ]
+        set -l dirty "$gray$nu"
+        set git_info "$dirty $git_info"
+      end
+
+      set -l nm (_number_modified_files)
+      if [ $nm -gt 0 ]
+        set -l dirty "$yellow$nm"
         set git_info "$dirty $git_info"
       end
     end
