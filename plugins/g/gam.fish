ceb01839dc1cf3d48b5b3b0abb853d16148b3dea
xixixao <xixixao@seznam.cz>
2013-12-21 20:37:04 +0100

    'Fixed print out of status in gim and gam'

--- plugins/g/gam.fish
+++ plugins/g/gam.fish
@@ -1,7 +1,7 @@
 function gam
   set -l stat (git --no-pager diff --color --stat HEAD)
   [ "$stat" != "" ]
-  and echo $stat
+  and _print_multiline $stat
   and read -p "set_color green; echo -n 'OK? '; set_color normal"
   git commit -a (_suffix_message $argv)
 end
