f5feb3e75190765748d65ebbc7738e19820a7bf8
xixixao <xixixao@seznam.cz>
2014-04-07 23:20:43 -0400

    Removed deprecated custom plugin for s

--- custom/plugins/sublime/s.fish
+++
@@ -1,5 +0,0 @@
-function s --description 'Open Sublime Text 2'
-  if test -d "/Applications/Sublime Text 2.app"
-    "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" $argv
-  end
-end
